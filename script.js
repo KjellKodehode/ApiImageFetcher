546736e137166816b52a27de462656c1cd6557c1
KjellKodeHode <kjell.arne.kodehode@gmail.com>
2022-04-29 13:07:37 +0200

    implemented enter key to get images

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+// get images clicking button
+document.getElementById("btn-get").addEventListener("click", fetchImg) 
+
+// get images clicking "Enter" key 
+addEventListener("keydown", (e)=> {
+    if(e.key !== "Enter") return
+    fetchImg()
+})
+
 // create images
-document.getElementById("btn-get").addEventListener("click", ()=>{
+function fetchImg(){
     fetch(`https://picsum.photos/${inputX.value}/${inputY.value}`)
     .then(imgRes => {
         const img = document.createElement ("img")
         img.src = imgRes.url
         document.body.append(img)
     })
-})
+}
 
 // refresh page
 document.getElementById("btn-clr").addEventListener("click", ()=>{
     window.location.reload()
+    console.log("refresh")
 })
