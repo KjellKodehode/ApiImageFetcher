177ef4f4788a6fed5203e660d4ca202ec17c7963
KjellKodeHode <kjell.arne.kodehode@gmail.com>
2022-04-29 13:42:41 +0200

    scroll to top button

--- script.js
+++ script.js
@@ -22,3 +22,31 @@ document.getElementById("btn-clr").addEventListener("click", ()=>{
     window.location.reload()
     console.log("refresh")
 })
+
+// scroll to top button
+const scrollTop = function() {
+    const scrollBtn = document.createElement("button");
+    scrollBtn.innerHTML ="🔝";
+    scrollBtn.setAttribute("id", "scroll-btn");
+    document.body.appendChild(scrollBtn);
+    
+    const scrollBtnDisplay = function() {
+        window.scrollY > window.innerHeight
+        ? scrollBtn.classList.add("show")
+        : scrollBtn.classList.remove("show");
+    }
+    window.addEventListener("scroll", scrollBtnDisplay)
+
+    const scrollWindow = function() {
+        if(window.scrollY !=0) {
+            setTimeout(function() {
+                window.scrollTo(0, window.scrollY - 50);
+                scrollWindow()
+            }, 10);
+        }
+    }
+    scrollBtn.addEventListener("click", scrollWindow)
+};
+scrollTop();
+// this changes the scrolling behavior to "smooth"
+// window.scrollTo({ top: 0, behavior: 'smooth' });
